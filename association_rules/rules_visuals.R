36564656ab0ccf6f8281c25599aa66117e253444
Prateek <59927461+prateek025@users.noreply.github.com>
2022-04-22 22:07:03 -0400

    Add files via upload

---
+++ association_rules/rules_visuals.R
@@ -0,0 +1,29 @@
+#importing useful libraries
+library(dplyr)
+library(arules)
+library(visNetwork)
+library(arulesViz)
+library(plotly)
+library(igraph)
+
+#importing the result file developed from running ARules on grocery orders
+df1 = read.csv(file.choose())
+df2 = df1[,1:2]
+
+#defining the unique nodes and edges for the graph
+df_nds = data.frame("BP" = as.character(c(df2[,"Antecedent"], df2[,"Consequent"])))
+df_nds = data.frame("BP" = unlist(df2, use.names = F))
+nds = unique(df_nds)
+
+nods = data.frame(id = nds$BP, label = nds$BP, title = nds$BP) %>% arrange(id)
+edgs = data.frame(from = df2$Antecedent[], to = df2$Consequent[])
+
+#developing the network graph
+visNetwork(nods, edgs, height = '1000px', width = '100%', main = 'Products Purchased', size = 5) %>%
+  visIgraphLayout(layout = "layout_in_circle") %>%
+  visNodes(size = 10) %>%
+  visLegend() %>%
+  visEdges(smooth = F) %>%
+  visOptions(highlightNearest = list(enabled = T, hover = T), nodesIdSelection = T, collapse = T, manipulation =  T) %>%
+  visInteraction(tooltipDelay = 0) %>%
+  visPhysics(solver = "barnesHut", maxVelocity = 1, forceAtlas2Based = list(gravitationalConstant = -20))
